f9c5e6835e2d6fdf682a7eb8604d627694642925
Arm0216 <arman.poghosyan16@gmail.com>
2023-02-03 18:39:54 +0400

    Added prettierrc file

--- selectors/HomeSelectors/HomeSelectors.ts
+++ selectors/HomeSelectors/HomeSelectors.ts
@@ -1,21 +1,21 @@
-import {Locator, Page} from "@playwright/test";
+import { Locator, Page } from '@playwright/test';
 
 class HomeSelectors {
-    page: Page
-    constructor(page:Page) {
-        this.page = page
+    page: Page;
+    constructor(page: Page) {
+        this.page = page;
     }
-    portfolioAreaLocator(): Locator{
+    portfolioAreaLocator(): Locator {
         return this.page.locator('[class*="portfolio puzzle"]');
     }
 
-    portfoliosLocator(): Locator{
+    portfoliosLocator(): Locator {
         return this.portfolioAreaLocator().locator('article');
     }
 
     portfolioHoverText(text?: string): Locator {
-        return this.portfolioAreaLocator().locator('.project_inner a', {hasText: text})
+        return this.portfolioAreaLocator().locator('.project_inner a', { hasText: text });
     }
 }
 
-export default HomeSelectors
+export default HomeSelectors;
--- tests/HomeTest/home.spec.ts
+++ tests/HomeTest/home.spec.ts
@@ -1,27 +1,26 @@
-import {test} from "../fixtures/fixtures.spec";
-const parameters: JSON = require('./test_parameters.json')
+import { test } from '../fixtures/fixtures.spec';
+import parameters from './test_parameters.json';
 
+test.describe('Home page functionality', () => {
+    test.beforeEach(async ({ homePage }) => {
+        await homePage.goTo();
+    });
 
-test.describe('Home page functionality', ()=>{
-    test.beforeEach(async ({homePage})=>{
-        await homePage.goTo()
-    })
-
-    test('Verify portfolios hover functionality', async ({homePage, assertion, common})=>{
+    test('Verify portfolios hover functionality', async ({ homePage, assertion, common }) => {
         await homePage.scrollOnPortfolioArea();
-        for (const {index, value} of common.getLoopIndexAndValue(parameters['portfolios'])){
-            await homePage.hoverOnPortfolio(index)
-            await assertion.verifyPortfolioHoverTextVisibility(value)
+        for (const { index, value } of common.getLoopIndexAndValue(parameters.portfolios)) {
+            await homePage.hoverOnPortfolio(index);
+            await assertion.verifyPortfolioHoverTextVisibility(value);
         }
-    })
+    });
 
-    parameters['portfolios'].forEach((value, index)=>{
-        test(`Verify ${value} portfolio click functionality`, async ({homePage, assertion})=>{
-            const url = await homePage.getUrlPathOfPortfolio(value)
+    parameters.portfolios.forEach((value, index) => {
+        test(`Verify ${value} portfolio click functionality`, async ({ homePage, assertion }) => {
+            const url = await homePage.getUrlPathOfPortfolio(value);
             await homePage.scrollOnPortfolioArea();
-            await homePage.clickOnPortfolio(index)
-            await assertion.verifyPageUrl(url)
-            await assertion.verifyProjectTitle(value)
-        })
-    })
-})
+            await homePage.clickOnPortfolio(index);
+            await assertion.verifyPageUrl(url);
+            await assertion.verifyProjectTitle(value);
+        });
+    });
+});
